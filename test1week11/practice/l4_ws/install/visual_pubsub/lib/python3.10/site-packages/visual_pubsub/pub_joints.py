6678b4c73a06e712798b5c23f66435e05653d236
LuckyStar291 <luz.soria.carvajal@gmail.com>
2025-10-16 04:27:09 -0400

    CINEMATICA INVERSA

--- test1week11/practice/l4_ws/install/visual_pubsub/lib/python3.10/site-packages/visual_pubsub/pub_joints.py
+++ test1week11/practice/l4_ws/install/visual_pubsub/lib/python3.10/site-packages/visual_pubsub/pub_joints.py
@@ -1,26 +1,24 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import JointState
+from sensor_msgs.msg import JointState # <- tipo de mensaje con 'name', 'position', 'velocity', 'effort'
 from builtin_interfaces.msg import Time
 
 
 class JointStatePublisher(Node):
 
     def __init__(self):
-        super().__init__('joint_state_publisher')
-        self.publisher_ = self.create_publisher(JointState, 'joint_states', 10)
+        super().__init__('joint_state_publisher')  # nombre del nodo
+        self.publisher_ = self.create_publisher(JointState, 'joint_states', 10) # tópico
         self.timer = self.create_timer(
             0.1, self.publish_joint_states)  # Publish every 0.1s
 
     def publish_joint_states(self):
         msg = JointState()
         msg.header.stamp = self.get_clock().now().to_msg()  # Add timestamp
-        msg.name = [
-            'arm_joint1', 'arm_joint2', 'arm_joint3', 'arm_joint4',
-            'arm_joint5', 'arm_joint6', 'right_gripper_finger_joint',
-            'left_gripper_finger_joint'
+        msg.name = [ # <- nombres de juntas que se publican (deben existir en tu URDF si quieres que RViz se anime)
+            'q1', 'q2', 'q3'
         ]
-        msg.position = [1.0, -0.5, 0.1, 0.2, -0.7, 1.1, -0.011, 0.012]
+        msg.position = [90, 90, 90] # <- posiciones de las juntas
 
         self.publisher_.publish(msg)
         self.get_logger().info(
