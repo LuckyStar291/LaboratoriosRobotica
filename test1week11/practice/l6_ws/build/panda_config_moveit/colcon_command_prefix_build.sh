a7557aa1502004bf2e4599c93b033f57b1f6c235
LuckyStar291 <luz.soria.carvajal@gmail.com>
2025-10-16 05:23:36 -0400

    moveit 2 panda_config_moveit la venganza >:v

---
+++ test1week11/practice/l6_ws/build/panda_config_moveit/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/luz/Escritorio/Robotica/test1week11/practice/l6_ws/install/robot_description/share/robot_description/package.sh"
