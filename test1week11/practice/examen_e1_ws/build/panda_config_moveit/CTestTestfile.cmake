5a87fcc6e4ca7c893f9e27bbc9e54129182ad8ff
LuckyStar291 <luz.soria.carvajal@gmail.com>
2025-10-18 20:08:34 -0400

    todo good?

---
+++ test1week11/practice/examen_e1_ws/build/panda_config_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/src/ros2_tutorials/panda_config_moveit
+# Build directory: /home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ test1week11/practice/examen_e1_ws/build/panda_config_moveit/cmake_install.cmake
@@ -0,0 +1,125 @@
+# Install script for directory: /home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/src/ros2_tutorials/panda_config_moveit
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/install/panda_config_moveit")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/panda_config_moveit")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/panda_config_moveit")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit/environment" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_environment_hooks/ament_prefix_path.dsv")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit/environment" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_environment_hooks/path.dsv")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_environment_hooks/local_setup.bash")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_environment_hooks/local_setup.sh")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_environment_hooks/local_setup.zsh")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_environment_hooks/local_setup.dsv")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_environment_hooks/package.dsv")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_index/share/ament_index/resource_index/packages/panda_config_moveit")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit/cmake" TYPE FILE FILES
+    "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_core/panda_config_moveitConfig.cmake"
+    "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/ament_cmake_core/panda_config_moveitConfig-version.cmake"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/src/ros2_tutorials/panda_config_moveit/package.xml")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE DIRECTORY FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/src/ros2_tutorials/panda_config_moveit/launch" REGEX "/setup\\_assistant\\.launch$" EXCLUDE)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE DIRECTORY FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/src/ros2_tutorials/panda_config_moveit/config")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/panda_config_moveit" TYPE FILE FILES "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/src/ros2_tutorials/panda_config_moveit/.setup_assistant")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/luz/Escritorio/Robotica/test1week11/practice/examen_e1_ws/build/panda_config_moveit/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
